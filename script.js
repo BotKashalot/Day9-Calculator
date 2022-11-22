d48862344ab352c3d5620372217bc88c299f3812
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-21 21:13:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//HTML
+//contianer with id
+
+// JS
+// TODO
+// init state
+// number 1 - 9, button.AC button, +/-, %, (+ - * /)
+function makeNumKeys(){
+    for(let i = 1; i < 10; i++){
+        console.log(i)
+    }
+}
+
+
+
+
+// functions for all operations:
+// add, sub, mul, div. equal, percentage, negative, dot
+// ac and c
+// display
